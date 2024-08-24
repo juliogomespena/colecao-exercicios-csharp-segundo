ac2d304dd49937bc1fe3feae723469245499b0bb
Julio Gomes Pena <juliogomespena@gmail.com>
2024-08-24 09:36:44 -0300

    Criar colecao de exercícios 01 - curso 02

---
+++ C# Aplicando a Orientação a Objetos - Exercicios 01/ContaBancaria.cs
@@ -0,0 +1,19 @@
+﻿class ContaBancaria
+{
+
+    public int id;
+    public string titular;
+    public decimal saldo;
+    public string senha;
+
+    public void ExibirInformacoes()
+    {
+
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Titular: {titular}");
+        Console.WriteLine($"Saldo: {saldo}");
+        Console.WriteLine($"Senha: {senha}");
+
+    }
+
+}
---
+++ C# Aplicando a Orientação a Objetos - Exercicios 01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+ContaBancaria conta01 = new();
+
+conta01.id = 1;
+conta01.titular = "Julio Gomes Pena";
+conta01.saldo = 9521312.12m;
+conta01.senha = "980021";
+
+conta01.ExibirInformacoes();
